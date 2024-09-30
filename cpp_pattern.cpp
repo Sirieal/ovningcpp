a082fbdd857135c164105cbf6cb88bd6b47513a5
Sirieal <sirilak-kampan@outlook.com>
2024-09-30 20:35:45 +0200

    ovning c++

---
+++ cpp_pattern.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "  _____        ___   _____        _         _  " << std::endl;
+    std::cout << " /  ___|      /  /  /  ___|      | |       | |     " << std::endl;
+    std::cout << "|  /         /  /  |  /        __| |__   __| |__    " << std::endl;
+    std::cout << "| |         /  /   | |        |__   __| |__   __|      " << std::endl;
+    std::cout << "|  \\___    /  /    |  \\___       | |       | |   " << std::endl;
+    std::cout << " \\_____|  /__/      \\_____|      |_|       |_|  " << std::endl;
+
+    return 0;
+}
---
+++ myinfo.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Mitt namn: Namn Efternamn" << std::endl;
+    std::cout << "Min address: Address" << std::endl;
+    return 0;
+}
